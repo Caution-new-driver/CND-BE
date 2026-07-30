7f8d89804ab125d21e467440bd09ab4c5dd4fe13
kimreenact <reenact14@gmail.com>
2026-07-30 17:13:36 +0900

    feat: OpenAI, Cloudinary 외부 서비스 클라이언트 세팅 (b2)

--- src/main/java/com/nextrun/cndbe/Application.java
+++ src/main/java/com/nextrun/cndbe/Application.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Application {ac
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
